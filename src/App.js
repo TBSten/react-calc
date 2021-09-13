ee1182abf6ee39fbf6575924dc6a7957610dca5f
TBSten <81161390+TBSten@users.noreply.github.com>
2021-09-13 20:39:40 +0900

    ベース作成

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./components/Button" ;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calc">
+      <header>電卓</header>
+      <div className="display">dipslay</div>
+      <div className="input">
+        <div className="numbers">
+          {
+            [1,2,3,4,5,6,7,8,9,0,".","="].map(el=>(
+              <Button text={el}/>
+            ))
+          }
+        </div>
+        <div className="operators">
+          {
+            ["/","*","-","+"].map(el=>(
+              <Button text={el}/>
+            ))
+          }
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
