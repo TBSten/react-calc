0ce7ef36b6a50af4da1313515c77aadf0f5b4b17
TBSten <81161390+TBSten@users.noreply.github.com>
2021-09-13 21:35:09 +0900

    Revert "表示窓追加"
    This reverts commit 00cf8a1c5575ee6b82b8cab2ade987932fcb9451.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ function App() {
   return (
     <div className="calc">
       <header>電卓</header>
-      <div className="display">ここに計算結果表示</div>
+      <div className="display">dipslay</div>
       <div className="input">
         <div className="numbers">
+          {
+            [1,2,3,4,5,6,7,8,9,0,".","="].map(el=>(
+              <Button text={el}/>
+            ))
+          }
         </div>
         <div className="operators">
+          {
+            ["/","*","-","+"].map(el=>(
+              <Button text={el}/>
+            ))
+          }
         </div>
       </div>
     </div>
