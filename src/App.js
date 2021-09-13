00cf8a1c5575ee6b82b8cab2ade987932fcb9451
TBSten <81161390+TBSten@users.noreply.github.com>
2021-09-13 20:44:13 +0900

    表示窓追加

--- src/App.js
+++ src/App.js
@@ -4,21 +4,11 @@ function App() {
   return (
     <div className="calc">
       <header>電卓</header>
-      <div className="display">dipslay</div>
+      <div className="display">ここに計算結果表示</div>
       <div className="input">
         <div className="numbers">
-          {
-            [1,2,3,4,5,6,7,8,9,0,".","="].map(el=>(
-              <Button text={el}/>
-            ))
-          }
         </div>
         <div className="operators">
-          {
-            ["/","*","-","+"].map(el=>(
-              <Button text={el}/>
-            ))
-          }
         </div>
       </div>
     </div>
